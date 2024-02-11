dcb4cc139a335ad702b6c88e877a4f10d0538ab0
fedi-nabli <fedinabli@gmail.com>
2024-02-11 09:14:25 +0100

    Created the token structures

--- compiler.h
+++ compiler.h
@@ -2,6 +2,7 @@
 #define FODOCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
 enum
 {
@@ -9,11 +10,55 @@ enum
   COMPILER_FAILED_WITH_ERRORS
 };
 
+// Token types enum
+enum
+{
+  TOKEN_TYPE_IDENTIFIER,
+  TOKEN_TYPE_KEYWORD,
+  TOKEN_TYPE_OPERATOR,
+  TOKEN_TYPE_SYMBOL,
+  TOKEN_TYPE_NUMBER,
+  TOKEN_TYPE_STRING,
+  TOKEN_TYPE_COMMENT,
+  TOKEN_TYPE_NEWLINE
+};
+
+struct pos
+{
+  int line;
+  int col;
+  const char* filename;
+};
+
+struct token
+{
+  int type;
+  int flags;
+
+  union
+  {
+    char cval;
+    const char* sval;
+    unsigned int inum;
+    unsigned long lnum;
+    unsigned long long llnum;
+    void* any;
+  };
+
+  // True if there is whitespace between the token and the next token
+  // i.e "* a" for operator token "*" would mean whitespace would be set for token "a"
+  bool whitespace;
+
+  // (5+10+20)
+  const char* between_brackets;
+};
+
 struct compile_process
 {
   // The flags in regards to how this file should be compiled
   int flags;
 
+  struct pos pos;
   struct compile_process_input_file
   {
     FILE* fp;
