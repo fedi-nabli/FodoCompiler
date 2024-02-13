ad13b46c06b575d98618a4734da5f3226a5bfc8d
fedi-nabli <fedinabli@gmail.com>
2024-02-13 19:25:53 +0100

    Creating number, identifer and string nodes

--- compiler.h
+++ compiler.h
@@ -285,5 +285,18 @@ struct vector* lex_process_tokens(struct lex_process* process);
 
 // Token helper functions
 bool token_is_keyword(struct token* token, const char* value);
+bool token_is_symbol(struct token* token, char c);
+bool token_is_nl_comment_or_newline_seperator(struct token* token);
+
+// < Node functions start
+
+void node_set_vector(struct vector* vec, struct vector* root_vec);
+void node_push(struct node* node);
+struct node* node_peek_or_null();
+struct node* node_peek();
+struct node* node_pop();
+struct node* node_create(struct node* _node);
+
+// > Node function end
 
 #endif
