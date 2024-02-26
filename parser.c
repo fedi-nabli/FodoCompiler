40882ffeaceea9a6b60a4ec62beabaa858fbeea3
fedi-nabli <fedinabli@gmail.com>
2024-02-26 13:11:10 +0100

    Implementing the variable node scope offsets - Part 3

--- parser.c
+++ parser.c
@@ -43,6 +43,11 @@ struct parser_scope_entity* parser_scope_last_entity_stop_global_scope()
   return scope_last_entity_stop_at(current_process, current_process->scope.root);
 }
 
+struct parser_scope_entity* parser_scope_last_entity()
+{
+  return scope_last_entity(current_process);
+}
+
 // > Parser scope end
 
 // < History start
@@ -51,7 +56,8 @@ enum
 {
   HISTORY_FLAG_INSIDE_UNION = 0b00000001,
   HISTORY_FLAG_IS_UPWARD_STACK = 0b00000010,
-  HISTORY_FLAG_IS_GLOBAL_SCOPE = 0b00000100
+  HISTORY_FLAG_IS_GLOBAL_SCOPE = 0b00000100,
+  HISTORY_FLAG_INSIDE_STRUCTURE = 0b00001000
 };
 
 /**
@@ -606,9 +612,24 @@ void make_variable_node(struct datatype* dtype, struct token* name_token, struct
   node_create(&(struct node){.type=NODE_TYPE_VARIABLE, .var.name=name_str, .var.type=*dtype, .var.val=value_node});
 }
 
-int parser_scope_offset_for_global(struct node* node, struct history* history)
+void parser_scope_offset_for_global(struct node* node, struct history* history)
 {
-  return 0;
+}
+
+void parser_scope_offset_for_structure(struct node* node, struct history* history)
+{
+  int offset = 0;
+  struct parser_scope_entity* last_entity = parser_scope_last_entity();
+  if (last_entity)
+  {
+    offset += last_entity->stack_offset + last_entity->node->var.type.size;
+    if (variable_node_is_primitive(node))
+    {
+      node->var.padding = padding(offset, node->var.type.size);
+    }
+
+    node->var.aoffset = offset + node->var.padding;
+  }
 }
 
 void parser_scope_offset_for_stack(struct node* node, struct history* history)
@@ -637,6 +658,12 @@ void parser_scope_offset(struct node* node, struct history* history)
   if (history->flags & HISTORY_FLAG_IS_GLOBAL_SCOPE)
   {
     parser_scope_offset_for_global(node, history);
+    return;
+  }
+
+  if (history->flags & HISTORY_FLAG_INSIDE_STRUCTURE)
+  {
+    parser_scope_offset_for_structure(node, history);
     return;
   }
 
