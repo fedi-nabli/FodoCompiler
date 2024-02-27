779d2fa5998475a041e83e37b97eafa9fea5aafb
fedi-nabli <fedinabli@gmail.com>
2024-02-27 21:53:00 +0100

    Implementing the function structure

--- compiler.h
+++ compiler.h
@@ -208,6 +208,16 @@ enum
 
 // > Symnol type enum end
 
+// < Function flags enum start
+
+enum
+{
+  // The flag is set for native functions
+  FUNCTION_NODE_FLAG_IS_NATIVE = 0b00000001
+};
+
+// > Function flags enum end
+
 #define TOTAL_OPERATOR_GROUPS 14
 #define MAX_OPERATORS_IN_GROUP 12
 
@@ -480,6 +490,32 @@ struct node
       // A pointer to the largest variable node in the statements vector
       struct node* largest_var_node;
     } body;
+
+    struct function
+    {
+      // Special flags
+      int flags;
+      // Return type i.e void, int, long ....
+      struct datatype rtype;
+
+      // I.e function name "main"
+      const char* name;
+
+      struct function_arguments
+      {
+        // Vector of struct node* must be NODE_TYPE_VARIABLE
+        struct vector* vector;
+
+        // How much to add to the EBP to find the first argument
+        size_t stack_addition;
+      } args;
+
+      // Pointer to the function body node, NULL if this is a function prototype
+      struct node* body_n;
+
+      // The stack size for all variables inside this function
+      size_t stack_size;
+    } func;
   };
 
   union
