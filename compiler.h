7b285e3dc57d046d98d7db2c9e45a7227f9bdf40
fedi-nabli <fedinabli@gmail.com>
2024-03-01 21:49:14 +0100

    Implementing return statement

--- compiler.h
+++ compiler.h
@@ -525,6 +525,12 @@ struct node
 
     struct statement
     {
+      struct return_stmt
+      {
+        // The expression of the return
+        struct node* exp;
+      } return_stmt;
+
       struct if_stmt
       {
         // if (COND) { // body }
@@ -630,6 +636,7 @@ void make_bracker_node(struct node* node);
 void make_body_node(struct vector* body_vec, size_t size, bool padded, struct node* largest_var_node);
 void make_struct_node(const char* name, struct node* body_node);
 void make_function_node(struct datatype* ret_type, const char* name, struct vector* arguments, struct node* body_node);
+void make_return_node(struct node* exp_node);
 void make_if_node(struct node* cond_node, struct node* body_node, struct node* next_node);
 void make_else_node(struct node* body_node);
 struct node* node_from_sym(struct symbol* sym);
