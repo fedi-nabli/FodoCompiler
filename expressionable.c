f6b30b6deab74c48ed2c77290e38cfe4beb8b741
fedi-nabli <fedinabli@gmail.com>
2024-03-24 20:00:36 +0100

    Creating the expressionable system - Part 2

--- expressionable.c
+++ expressionable.c
@@ -31,6 +31,11 @@ int expressionable_parse_single(struct expressionable* expressionable);
 
 // > Expressionable global functions end
 
+void expressionable_error(struct expressionable* expressionable, const char* str)
+{
+  assert(0 == 1 && str);
+}
+
 struct expressionable_callbacks* expressionable_callbacks(struct expressionable* expressionable)
 {
   return &expressionable->config.callbacks;
@@ -73,6 +78,13 @@ struct token* expressionable_peek_next(struct expressionable* expressionable)
   return vector_peek_no_increment(expressionable->token_vec);
 }
 
+struct token* expressionable_token_next(struct expressionable* expressionable)
+{
+  struct token* next_token = vector_peek_no_increment(expressionable->token_vec);
+  expressionable_ignore_nl(expressionable, next_token);
+  return vector_peek(expressionable->token_vec);
+}
+
 int expressionable_parse_number(struct expressionable* expressionable)
 {
   void* node_ptr = expressionable_callbacks(expressionable)->handle_number(expressionable);
@@ -85,6 +97,71 @@ int expressionable_parse_number(struct expressionable* expressionable)
   return 0;
 }
 
+int expressionable_parse_identifier(struct expressionable* expressionable)
+{
+  void* node_ptr = expressionable_callbacks(expressionable)->handle_identifier(expressionable);
+  if (!node_ptr)
+  {
+    return -1;
+  }
+
+  expressionable_node_push(expressionable, node_ptr);
+  return 0;
+}
+
+void expressionable_parse_for_operator(struct expressionable* expressionable)
+{
+  struct token* op_token = expressionable_peek_next(expressionable);
+  const char* op = op_token->sval;
+  void* node_left = expressionable_node_peek_or_null(expressionable);
+  if (!node_left)
+  {
+    #warning "Deal with unary"
+    return;
+  }
+
+  // Pop the operator token
+  expressionable_token_next(expressionable);
+
+  // Pop the left node
+  expressionable_node_pop(expressionable);
+
+  if (expressionable_peek_next(expressionable)->type == TOKEN_TYPE_OPERATOR)
+  {
+    if (S_EQ(expressionable_peek_next(expressionable)->sval, "("))
+    {
+      #warning "Parse for parentheses"
+    }
+    else if (is_unary_operator(expressionable_peek_next(expressionable)->sval))
+    {
+      #warning "Parse for unary"
+    }
+    else
+    {
+      expressionable_error(expressionable, "The operators are not expected for the given expression");
+    }
+  }
+  else
+  {
+    // Parse the right node
+    expressionable_parse_single(expressionable);
+  }
+
+  void* node_right = expressionable_node_pop(expressionable);
+  expressionable_callbacks(expressionable)->make_expression_node(expressionable, node_left, node_right, op);
+  void* exp_node = expressionable_node_pop(expressionable);
+  expressionable_node_push(expressionable, exp_node);
+}
+
+int expressionable_parse_exp(struct expressionable* expressionable, struct token* token)
+{
+  #warning "Parse parentheses"
+  #warning "Parse tenary"
+
+  expressionable_parse_for_operator(expressionable);
+  return 0;
+}
+
 int expressionable_parse_token(struct expressionable* expressionable, struct token* token, int flags)
 {
   int res = -1;
@@ -92,6 +169,14 @@ int expressionable_parse_token(struct expressionable* expressionable, struct tok
   {
     case TOKEN_TYPE_NUMBER:
       res = expressionable_parse_number(expressionable);
+      break;
+
+    case NODE_TYPE_IDENTIFIER:
+      res = expressionable_parse_identifier(expressionable);
+      break;
+
+    case NODE_TYPE_EXPRESSION:
+      res = expressionable_parse_exp(expressionable, token);
       break;
   }
 
