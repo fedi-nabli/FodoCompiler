88f9628708f6b55d05c23deefc0e3deafd25ac19
fedi-nabli <fedinabli@gmail.com>
2024-03-28 23:14:36 +0100

    Implementing the error macro

--- preprocessor/preprocessor.c
+++ preprocessor/preprocessor.c
@@ -527,6 +527,16 @@ bool preprocessor_token_is_warning(struct token* token)
   return (S_EQ(token->sval, "warning"));
 }
 
+bool preprocessor_token_is_error(struct token* token)
+{
+  if (!preprocessor_token_is_preprocessor_keyword(token))
+  {
+    return false;
+  }
+
+  return (S_EQ(token->sval, "error"));
+}
+
 void preprocessor_multi_value_insert_to_vector(struct compile_process* compiler, struct vector* value_token_vec)
 {
   struct token* value_token = preprocessor_next_token(compiler);
@@ -665,6 +675,12 @@ void preprocessor_handle_warning_token(struct compile_process* compiler)
   preprocessor_execute_warning(compiler, buffer_ptr(str_buf));
 }
 
+void preprocessor_handle_error_token(struct compile_process* compiler)
+{
+  struct buffer* str_buf = preprocessor_multi_value_string(compiler);
+  preprocessor_execute_error(compiler, buffer_ptr(str_buf));
+}
+
 int preprocessor_handle_hashtag_token(struct compile_process* compiler, struct token* token)
 {
   bool is_preprocessed = false;
@@ -685,6 +701,11 @@ int preprocessor_handle_hashtag_token(struct compile_process* compiler, struct t
   {
     preprocessor_handle_warning_token(compiler);
     is_preprocessed = true;
+  }
+  else if (preprocessor_token_is_error(next_token))
+  {
+    preprocessor_handle_error_token(compiler);
+    is_preprocessed = false;
   }
 
   return is_preprocessed;
--- test.c
+++ test.c
@@ -1,3 +1,4 @@
 #define ABC(x, y)
 #undef ABC
 #warning "Hello world"
+#error "hello world"
