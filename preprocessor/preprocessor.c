db12419e35e5de5c8f24d0638c6e969e3bbab4fa
fedi-nabli <fedinabli@gmail.com>
2024-04-04 20:20:06 +0100

    Implementing the parentheses node in the preprocessor

--- preprocessor/preprocessor.c
+++ preprocessor/preprocessor.c
@@ -1391,6 +1391,11 @@ int preprocessor_evaluate_unary(struct compile_process* compiler, struct preproc
   return res;
 }
 
+int preprocessor_evaluate_parentheses(struct compile_process* compiler, struct preprocessor_node* node)
+{
+  return preprocessor_evaluate(compiler, node->parenthesis_node.exp);
+}
+
 int preprocessor_evaluate(struct compile_process* compiler, struct preprocessor_node* root_node)
 {
   struct preprocessor_node* current = root_node;
@@ -1412,6 +1417,10 @@ int preprocessor_evaluate(struct compile_process* compiler, struct preprocessor_
 
     case PREPROCESSOR_EXPRESSION_NODE:
       result = preprocessor_evaluate_exp(compiler, current);
+      break;
+
+    case PREPROCESSOR_PARENTHESES_NODE:
+      result = preprocessor_evaluate_parentheses(compiler, current);
       break;
   }
 
